79cb7ab9f92656433e4a39410559a0f4f701bd9b
Amar Raul <143498791+MyResearchRoom@users.noreply.github.com>
2026-09-01 17:30:10 +0530

    Update index.js

--- src/assets/video/index.js
+++ src/assets/video/index.js
@@ -1,6 +1,6 @@
-import V2 from "./v2mel.mp4"
-import V3 from "./v3mel.mp4"
-import V4 from "./v4mel.mp4"
+import V2 from "./V2mel.mp4"
+import V3 from "./V3mel.mp4"
+import V4 from "./V4mel.mp4"
 import V5 from "./v5mel.mp4"
 import V6 from "./v6mel.mp4"
 
